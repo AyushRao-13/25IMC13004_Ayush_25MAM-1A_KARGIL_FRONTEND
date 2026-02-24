a73907f718228c230e3746a3262af169af65271c
AyushRao-13 <ayushyadav13701@gmail.com>
2026-02-24 12:56:14 +0530

    exp-4

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
-let input=document.querySelector("input")
-let btn=document.querySelector("button")
-let list=document.querySelector("#list")
-btn.addEventListener("click", eventHandler)
+//callback - function that is passsed as a parameter into another function
 
-function eventHandler() {
-    let data= Input.value;
-    if(data=""){
-        alert("please enter your task")
-        return;
-    }
-
-    let li= document.createElement("li");
-    li.innerText=data;
-
-    list.appendChild(li);
-
-    input.value=""
-    li.addEventListener("click", function(){
-        list.removeChild(li)
-    })
-
+function welcome(){
+    console.log("welcome")
 }
+
+function greet (){
+    console.log("Greetings")
+}
+
+welcome(greet)
+
+function task1(call) {
+    setTimeout(() => {
+        console.log("task 1 is running");
+        call();
+    },1000)
+}
+
+function task2(call) {
+    setTimeout(() => {
+        console.log("task 2 is running");
+call()
+    },2000)
+}
+
+task1(function(){
+    task2(function(){
+        console.log("end of execution")
+    })
+})
+
+//solution to callback hell is promises
+
+new Promise((resolve, reject)=> {
+    let num=10;
+    if(num%2==0){
+        res("number is even")
+    }
+    else{
+        rej("number is odd")
+    }
+})
+
+Promise
+.then((msg)=> console.log(msg))
+.catch((err)=>console.log(err))
+.finally(()=>console.log("Finally method"))
